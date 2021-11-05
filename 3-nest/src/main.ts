26669801014e2b6bcd46e0ce36a4eadd20c08fa7
nethaniapedrano <tan000pedrano@gmail.com>
2021-11-05 13:07:43 +0800

    Angular exercise

--- 3-nest/src/main.ts
+++ 3-nest/src/main.ts
@@ -1,12 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { REFUSED } from 'dns';
 import { AppModule } from './app.module';
-import * as admin from 'firebase-admin';
-const DB_CREDENTIALS = require("../my-key.json");
+
 async function bootstrap() {
-  admin.initializeApp({
-    credential:admin.credential.cert(DB_CREDENTIALS),
-  });
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
